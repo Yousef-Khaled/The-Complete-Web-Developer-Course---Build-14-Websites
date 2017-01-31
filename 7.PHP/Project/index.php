09c6a8c109aa969c1fe2fc82e487ff73983fc87d
Joe <yousef.enkidu0@gmail.com>
2017-01-31 22:02:38 +0200

    2en Commit

--- 7.PHP/Project/index.php
+++ 7.PHP/Project/index.php
@@ -31,10 +31,6 @@
           text-align: center;
         }
 
-        .btnC{
-          margin-left: 38%;
-          margin-top: 10px; 
-        }
         .alert{
           margin-top: 5%;
           display: none;
@@ -50,15 +46,15 @@
           <div class="col-md-6 col-md-offset-3">
               <h1 class="white">Weather Predictor</h1>
               <h4 class="white">Enter your city below to get the weather forcast.</h4>
- 
-              <form>
-                <div class = "form-group">
+              
+ 			 <div class = "form-group">
                 <input type="text" name="City" class="form-control" placeholder="Eg. Cario , Paris ..." id = "ab">
-                </div>
-                <button type="button" class="btn btn-lg btn-success btnC" id = "bt">Find My Weather</button>  
-              </form>
+             </div>
+            <div style="margin-left: 33%;">
+            <button type="button" class="btn btn-lg btn-success"  id = "bt">Find My Weather</button>  
+            </div>
             <div class="alert alert-info" id = "als"></div>
-            <div class="alert alert-danger" id = "alf">Please try again!</div>
+            <div class="alert alert-danger" id = "alf">Please enter a valid City!</div>
             <div class="alert alert-danger" id = "aln">Please enter a city!</div>
           </div>
         </div>
@@ -68,30 +64,57 @@
   </body>
   <script type="jquery-3.1.0.min.js"></script>
   <script type="text/javascript">
-      $("#bt").click(function (event) {
-        
-        $(".alert").hide();
+  	
 
-        if ($("#ab").val() == ""){
-          $("#aln").fadeIn();      
+   $(document).keypress(function (e) {
+    if (e.which == 13) {
+        $("#bt").click();
+    }
+});
+
+  	$("#bt").click(function () {
+  		$("#als").fadeOut();
+  		$("#aln").fadeOut();
+  		$("#alf").fadeOut();
+  		if (!$("#ab").val())
+  		{
+  			$("#aln").fadeIn();
+  			return;
+  		}
+  		getWeather();
+  	});
+
+  	function getWeather()
+   {
+    
+    // Create our XMLHttpRequest object
+    //alert(code);
+    var hr = new XMLHttpRequest();
+    // Create some variables we need to send to our PHP file
+    var url = "scraper.php";
+    var vars = "City=" + $("#ab").val();
+    hr.open("POST", url, true);
+    // Set content type header information for sending url encoded variables in the request
+    hr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    var return_data = ""
+    // Access the onreadystatechange event for the XMLHttpRequest object
+    hr.onreadystatechange = function() {
+       if(hr.readyState == 4 && hr.status == 200) {
+          return_data = hr.responseText;
+          var weather = return_data;
+          if (return_data.length != 4){
+          	$("#als").html(return_data);
+          	$("#als").fadeIn();
+          }
+          else{
+          	$("#alf").fadeIn();
+          }
         }
-        else{
-          $.get('scraper.php?City=' + $("#ab").val() , function (data) {
-             var n = data.search(/Warning/i);
-             
-             if (n == -1){
-                $("#als").html(data);
-                $("#als").fadeIn();
-             }
-             else{
-                $("#alf").fadeIn();
-             }
-          });
-        }
-
-      });
-
-
+    }
+    // Send the data to PHP now... and wait for response to update the status div
+    hr.send(vars); // Actually execute the request
+   }
+   
   </script>
 
 </html>
--- 7.PHP/Project/scraper.php
+++ 7.PHP/Project/scraper.php
@@ -1,14 +1,11 @@
 <?php
 
-
-$s = $_GET['City'];
+$s = $_POST['City'];
 $s = str_replace(" ", "", $s);
 $page = file_get_contents('http://www.weather-forecast.com/locations/'.$s.'/forecasts/latest');
 preg_match('/3 Day Weather Forecast Summary:<\/b><span class="read-more-small"><span class="read-more-content"> <span class="phrase">(.*?)<\/span>/s', $page, $a);
 
-
 echo $a[1];
-
 ?>
 
 
